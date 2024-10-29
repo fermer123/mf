8531395035d637ee4a88df43d53fb3f696938f93
fermer123 <fermer_525@mail.ru>
2024-10-30 02:28:50 +0500

    fix

--- src/bootstrap.tsx
+++ src/bootstrap.tsx
@@ -5,17 +5,20 @@ import AppRouter from '@app/provider/AppRouter';
 import GlobalStyle from '@app/styles/global';
 import Theme from '@app/styles/Theme';
 
+import ErrorBoundary from './features/ErrorBoundary/ErrorBoundary';
 import {withProvider} from './shared/hooks/withProvider/withProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const Provider = withProvider();
 root.render(
   <Provider>
-    <Theme>
-      <GlobalStyle />
-      <BrowserRouter>
-        <AppRouter />
-      </BrowserRouter>
-    </Theme>
+    <ErrorBoundary>
+      <Theme>
+        <GlobalStyle />
+        <BrowserRouter>
+          <AppRouter />
+        </BrowserRouter>
+      </Theme>
+    </ErrorBoundary>
   </Provider>,
 );
